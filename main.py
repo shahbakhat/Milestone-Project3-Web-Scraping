1bb30379e086e726909dc81dec54b197d26deb26
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-01 21:34:18 +0000

    added tag to url according to the user selection

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
-#import pandas as pd
-#import requests
-#from bs4 import BeautifulSoup
-print ("Hello World")
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+#This st.selectbox(with desired options) creates a selection box with a list of options on streamlit server. """
+tags = st.selectbox('Choose a topic', ['love','humor','life','books'])
+#Putting the variable {tags} after /tag/ will add the selected option to url such as https://quotes.toscrape.com/tag/humor/
+url = f"https://quotes.toscrape.com/tag/{tags}/"
